8f4559931dca0805904968f45a36475325a64ca4
Houssine <ekhay1@outlook.com>
2024-09-01 12:54:49 -0400

    added data to the porject array to be used to render project component

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,3 +1,34 @@
+const projects = [
+  {
+    name: "PAC-Overflow",
+    technology: "MERN",
+    image: "./assets/packOverFlow1.png",
+    gitHub: "https://github.com/asw-afk/PAC-Overflow",
+    dUrl: "https://pac-overflow.onrender.com/",
+  },
+  {
+    name: "Text Editor",
+    technology: "MERN",
+    image: "./assets/textEditor1.png",
+    gitHub: "https://github.com/asw-afk/PAC-Overflow",
+    dUrl: "https://text-editor-pwa-rrm4.onrender.com",
+  },
+  {
+    name: "Safer Travels",
+    technology: "JS,CSS, API,",
+    image: "./assets/weatherDashboard1.png",
+    gitHub: "https://github.com/MTKRD/Safer-Travels",
+    dUrl: "https://mtkrd.github.io/Safer-Travels/",
+  },
+  {
+    name: "Task Board",
+    technology: "JS,jQuery,CSS",
+    image: "./assets/taskBoard1.png",
+    gitHub: "https://github.com/ekhay-hit/Tasks-Board",
+    dUrl: "https://ekhay-hit.github.io/Tasks-Board/",
+  },
+];
+
 export default function Portfolio() {
   return <h1>This is a Portifolio section</h1>;
 }
