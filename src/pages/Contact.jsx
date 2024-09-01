43eebbc6baa4169016d6a6af39f9b4fea1c8e965
Houssine <ekhay1@outlook.com>
2024-08-31 23:59:37 -0400

    added the form for contct section and styled it

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,31 @@
+import { Fragment } from "react";
+import "./styles/contact.css";
+
 export default function Contact() {
-  return <h1>This is contact section</h1>;
+  return (
+    <>
+      <form className="contact form-control">
+        <h2 className="col-12">Contact</h2>
+        <label className="label-control col-12">Name:</label>
+        <input
+          className=" input form-control"
+          type="text"
+          placeholder="Enter your name"
+        ></input>
+        <label className="label-control col-12">Email address:</label>
+        <input
+          className=" input form-control"
+          type="text"
+          placeholder="Enter your email"
+        ></input>
+        <label className="label-control col-12">Message:</label>
+        <textarea
+          className=" input form-control"
+          type="text"
+          placeholder="Enter your message"
+        ></textarea>
+        <button className="btn">Send</button>
+      </form>
+    </>
+  );
 }
