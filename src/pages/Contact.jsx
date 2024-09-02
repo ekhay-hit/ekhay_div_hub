5ffc220083a9f6e541b3c08c11eb628811c0d5a3
Houssine <ekhay1@outlook.com>
2024-09-02 11:05:52 -0400

    added the style to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,21 +4,21 @@ import "./styles/contact.css";
 export default function Contact() {
   return (
     <>
-      <form className="contact form-control">
-        <h2 className="col-12">Contact</h2>
-        <label className="label-control col-12">Name:</label>
+      <form className="contact ">
+        <h2 className="col-10">Contact :</h2>
+        <label className="label-control ">Name:</label>
         <input
           className=" input form-control"
           type="text"
           placeholder="Enter your name"
         ></input>
-        <label className="label-control col-12">Email address:</label>
+        <label className="label-control ">Email address:</label>
         <input
-          className=" input form-control"
+          className=" input form-control col-8"
           type="text"
           placeholder="Enter your email"
         ></input>
-        <label className="label-control col-12">Message:</label>
+        <label className="label-control">Message:</label>
         <textarea
           className=" input form-control"
           type="text"
